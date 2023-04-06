c50d77c16c355029f8a07e57eba850046a9447c7
Igor Matos <sarkan.igor@gmail.com>
2023-04-05 21:42:48 -0300

    Criado pagina home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Divider, Typography } from '@mui/material';
+import React from 'react';
+
+const ComponetName: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Typography variant="h2">Saldo</Typography>
+      <Divider />
+    </React.Fragment>
+  );
+};
+
+export default ComponetName;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DefaultLayout from '../config/layout/DefaultLayout';
-import Home from '../pages/Transaction';
+import Transaction from '../pages/Transaction';
+import Home from '../pages/Home';
 
 const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<DefaultLayout component={Home} />} />
+        <Route path="/transaction" element={<DefaultLayout component={Transaction} />} />
+        <Route path="/" element={<DefaultLayout component={Home} />} />
       </Routes>
     </BrowserRouter>
   );
